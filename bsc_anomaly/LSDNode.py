842d0f2dcdb552942be43077233e4be06da450dc
Schoenberg <kevin27@outlook.dk>
2023-03-10 13:53:26 +0100

    Added automatic simulation at key press

--- bsc_anomaly/LSDNode.py
+++ bsc_anomaly/LSDNode.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from geometry_msgs.msg import PoseWithCovarianceStamped
 import time
 
 
@@ -18,47 +19,61 @@ class LSDNode(Node):
     """
     def __init__(self):
         super().__init__('LSD')
-
-        self.drive = self.create_publisher(
-            Twist, 
-            '/cmd_vel', 
-            10)
         
         self.drive_ack = self.create_publisher(
             AckermannDriveStamped, 
             '/drive', 
             10)
+        
+        self.keysub = self.create_subscription(
+            String, 
+            '/resetpos', 
+            self.key_pressed_call_back, 
+            10)
+        
+        self.pose_pub = self.create_publisher(
+            PoseWithCovarianceStamped,
+            '/initialpose',
+            10
+        )
+        
+    def key_pressed_call_back(self,data):
+        if (data.data == 'k'):
+            print("v")
+            # speed 0
+            self.drive_ack_pub(0.0)
+            # set inital pos
+            self.set_initial_pose()
+            #
+            time.sleep(1.)
+            # speed 5
+            self.drive_ack_pub(5.0)
 
-    def listener_callback(self, msg):
-        #self.get_logger().info('')
-        a = 1 + 2
 
     def odom_callback(self, msg):
         #self.get_logger().info('%s' % msg.twist.twist)
         print(msg.twist.twist.linear)
-    
-    def drive_pub(self, speed):
-        vel = Twist()
-        vel.linear.x = speed
-        self.drive.publish(vel)
 
     def drive_ack_pub(self, speed):
         vel = AckermannDriveStamped()
         vel.drive.speed = speed
         self.drive_ack.publish(vel)
+
+    def set_initial_pose(self):
+        pose = PoseWithCovarianceStamped()
+        pose.pose.pose.position.x = -78.0
+        pose.pose.pose.position.y = -28.0
+        self.pose_pub.publish(pose)
     
     def __del__(self):
         # body of destructor
-        vel = Twist()
-        vel.linear.x = 0.
-        self.drive.publish(vel)
-
+        self.drive_ack_pub(0.0)
 
 
 def main(args=None):
     rclpy.init(args=args)
     LSDN = LSDNode()
-    LSDN.drive_ack_pub(5.0)
+    #LSDN.drive_ack_pub(5.0)
 
     rclpy.spin(LSDN)
 
--- bsc_anomaly/LoggerNode.py
+++ bsc_anomaly/LoggerNode.py
@@ -14,6 +14,7 @@ from numpy import linalg as LA
 from tf_transformations import euler_from_quaternion
 import csv
 import time
+from threading import Thread
 
 
 
@@ -24,13 +25,10 @@ class LoggerNode(Node):
     def __init__(self):
         super().__init__('Logger')
 
-        home = expanduser('~')
+        self.home = expanduser('~')
+
         #file = open(strftime(home+'/sim_ws/our_test_logs/wp-%Y-%m-%d-%H-%M-%S',gmtime())+'.csv', 'w')
-        print(home)
-        self.file = open(home+'/sim_ws/experimentlog.csv', 'w')
-        header = ['x', 'y', 'yaw', 'speed', 'time']
-        self.writer = csv.writer(self.file)
-        self.writer.writerow(header)
+        
         self.odom = self.create_subscription(
             Odometry, 
             '/ego_racecar/odom', 
@@ -44,6 +42,8 @@ class LoggerNode(Node):
             '/resetpos', 
             self.key_pressed_call_back, 
             10)
+        
+        self.doLog = False
 
     def __del__(self):
         # body of destructor
@@ -52,27 +52,46 @@ class LoggerNode(Node):
     def key_pressed_call_back(self,data):
         if (data.data == 'k'):
             print("v")
+            self.doLog = False
+            #open file
+            self.file = open(self.home+'/sim_ws/src/bsc_anomaly/test_results/longitude-'+ str(gmtime()[1:6]) +'.csv', 'w')
+            header = ['x', 'y', 'yaw', 'speed', 'time']
+            self.writer = csv.writer(self.file)
+            self.writer.writerow(header)
+
+            # sleep
+            time.sleep(0.5)
+            # log data
+            self.doLog = True
+            self.simStart = time.time()
+
+
 
     def save_waypoint_call_back(self,data):
-        #print("Logging")
-        quaternion = np.array([data.pose.pose.orientation.x, 
-                            data.pose.pose.orientation.y, 
-                            data.pose.pose.orientation.z, 
-                            data.pose.pose.orientation.w])
+        if self.doLog:
+            if self.simStart + 10 < time.time():
+                self.doLog = False
+                self.file.close()
+                return
+            
+            quaternion = np.array([data.pose.pose.orientation.x, 
+                                data.pose.pose.orientation.y, 
+                                data.pose.pose.orientation.z, 
+                                data.pose.pose.orientation.w])
 
-        euler = euler_from_quaternion(quaternion)
-        speed = LA.norm(np.array([data.twist.twist.linear.x, 
-                                data.twist.twist.linear.y, 
-                                data.twist.twist.linear.z]),2)
-        if data.twist.twist.linear.x>0.:
-            print (data.twist.twist.linear.x)
-        #print("",data.pose.pose.position.x)
-        self.file.write('%f, %f, %f, %f, %f\n' % (data.pose.pose.position.x,
-                                        data.pose.pose.position.y,
-                                        euler[2],
-                                        speed, time.time()))
-
-        
+            euler = euler_from_quaternion(quaternion)
+            speed = LA.norm(np.array([data.twist.twist.linear.x, 
+                                    data.twist.twist.linear.y, 
+                                    data.twist.twist.linear.z]),2)
+            if data.twist.twist.linear.x>0.:
+                print (data.twist.twist.linear.x)
+            #print("",data.pose.pose.position.x)
+            self.file.write('%f, %f, %f, %f, %f\n' % (data.pose.pose.position.x,
+                                            data.pose.pose.position.y,
+                                            euler[2],
+                                            speed, time.time()))
+            print("logging2")
+    
 
 
 def main(args=None):
